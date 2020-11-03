3d38e73f71eec67dc0ca3372835612b677e2a9d9
Melvin Edward <melvinedward98@gmail.com>
2020-11-03 22:04:02 +0800

    javafx

---
+++ src/main/java/FoodObserver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Melvin
+ */
+public interface FoodObserver {
+    public void update(double slider, int length, String orientation);
+    public void update(double scale);
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/* doesn't work with source level 1.8:
+module com.melvinedward.javafx_test {
+    requires javafx.controls;
+    exports com.melvinedward.javafx_test;
+}
+*/
